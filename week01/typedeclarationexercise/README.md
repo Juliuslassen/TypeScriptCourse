cf718814915afb318e3e74c302c1f475a745dfe0
Julius Lassen <julius5517@gmail.com>
2024-02-02 22:12:51 +0100

    week 1 done 🙅🏼

---
+++ week01/typedeclarationexercise/README.md
@@ -0,0 +1,7 @@
+1. Where do you find axios type declaration file?
+    - findes i node_modules: relativ path er : week01/node_modules/axios/index.d.ts
+2. What is the name of the type declaration file for axios?
+    - index.d.ts
+3. How do you install a type declaration file for a library that does not have one?
+    -  npm i @types/{library-name}
+
---
+++ week01/typedeclarationexercise/typeDeclaration.ts
@@ -0,0 +1,81 @@
+import axios, { AxiosError, AxiosResponse } from 'axios'
+
+interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: number;
+    website: string;
+    company: Company;
+}
+
+interface Address{
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+}
+
+interface Geo {
+    lat: string;
+    lng: string;
+}
+
+interface Company{
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+
+async function getUserOne(): Promise<User | AxiosError> {
+    const url: string = 'https://jsonplaceholder.typicode.com/users/1';
+
+    try {
+        const response: AxiosResponse<User> = await axios.get(url);
+        const user: User = response.data;
+        return user;
+    } catch (error) {
+        return error;
+    }
+}
+
+async function getAllUsers(): Promise<User[] | AxiosError>{
+    const url: string = 'https://jsonplaceholder.typicode.com/users';
+    return await axios.get<User[]>(url)
+    .then((response: AxiosResponse<User[]>) =>{
+        const userArray: User[] = response.data;
+        return userArray;
+    })
+    .catch((error: AxiosError) =>{
+        return error
+    })
+}
+
+function printTheUser(user: User): void{
+    console.log("Name: " + user.name);
+    console.log("userName: " +user.username);
+    console.log("email: " +user.email);
+    console.log("phonenumber: " +user.phone);
+}
+
+getUserOne().then((result) => {
+    if (result instanceof Error) {
+        console.error('Error:', result.message);
+    } else {
+        printTheUser(result);
+    }
+});
+
+
+getAllUsers().then((result) =>{
+    if(result instanceof Error) {
+        console.log('Error:', result.message)
+    } else {
+        result.forEach(element => {
+            printTheUser(element);
+        });
+    }
+});
