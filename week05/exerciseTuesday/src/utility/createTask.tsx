011d1a91f354146d089b0c4be07f5a04ac0550d9
Julius Lassen <julius5517@gmail.com>
2024-02-29 01:11:26 +0100

    z

---
+++ week05/exerciseTuesday/src/utility/createTask.tsx
@@ -0,0 +1,9 @@
+
+export const createTask = (name: string, decription: string, timeEstimation: number) => {
+    return {
+        name,
+        decription,
+        timeEstimation,
+        completed: false
+    };
+}
